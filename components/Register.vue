d13a634f0f81f093a9f15410d777e0c79235b1f4
Mikołaj Gawron <gawrmiko@gmail.com>
2023-12-05 19:11:37 +0100

    dodanie rejestracji

---
+++ components/Register.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="text-center text-[28px] mb-4 font-bold">Zarejestruj się</div>
+
+  <div class="px-6 pb-2">
+    <TextInput
+      placeholder="Pełna nazwa"
+      v-model:input="name"
+      inputType="text"
+      :autoFocus="true"
+      :error="errors && errors.name ? errors.name[0] : ''"
+    />
+  </div>
+
+  <div class="px-6 pb-2">
+    <TextInput
+      placeholder="Adres e-mail"
+      v-model:input="email"
+      inputType="email"
+      :error="errors && errors.email ? errors.email[0] : ''"
+    />
+  </div>
+
+  <div class="px-6 pb-2">
+    <TextInput
+      placeholder="Hasło"
+      v-model:input="password"
+      inputType="password"
+      :error="errors && errors.password ? errors.password[0] : ''"
+    />
+  </div>
+
+  <div class="px-6 pb-2">
+    <TextInput
+      placeholder="Potwierdź hasło"
+      v-model:input="confirmPassword"
+      inputType="password"
+      :error="errors && errors.confirmPassword ? errors.confirmPassword[0] : ''"
+    />
+  </div>
+
+  <div class="px-6 pb-2 mt-6">
+    <button
+      :disabled="!name || !email || !password || !confirmPassword"
+      :class="
+        !name || !email || !password || !confirmPassword
+          ? 'bg-gray-200'
+          : 'bg-[#bc2cf0]'
+      "
+      @click="register()"
+      class="w-full text-[17px] font-semibold text-white bg-[#bc2cf0] py-3 rounded-sm"
+    >
+      Zarejestruj się
+    </button>
+  </div>
+</template>
+
+<script setup>
+const { $userStore, $generalStore } = useNuxtApp();
+
+let name = ref(null);
+let email = ref(null);
+let password = ref(null);
+let confirmPassword = ref(null);
+let errors = ref(null);
+
+const register = async () => {
+  errors.value = null;
+
+  try {
+    await $userStore.getTokens();
+    await $userStore.register(
+      name.value,
+      email.value,
+      password.value,
+      confirmPassword.value
+    );
+    await $userStore.getUser();
+    await $generalStore.getRandomUsers("suggested");
+    await $generalStore.getRandomUsers("following");
+    $generalStore.isLoginOpen = false;
+  } catch (error) {
+    console.log(error);
+    errors.value = error.response.data.errors;
+  }
+};
+</script>
