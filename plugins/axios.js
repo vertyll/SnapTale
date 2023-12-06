a96ea15719f5c1de428f3e01e6d931162eb1e057
Mikołaj Gawron <gawrmiko@gmail.com>
2023-12-06 10:21:32 +0100

    dodanie do axiosa withXSRFToken

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,13 +1,13 @@
-import axios from "axios"
+import axios from "axios";
 
 export default defineNuxtPlugin((NuxtApp) => {
+  axios.defaults.withCredentials = true;
+  axios.defaults.withXSRFToken = true;
+  axios.defaults.baseURL = "http://localhost:8000";
 
-    axios.defaults.withCredentials = true;
-    axios.defaults.baseURL = 'http://localhost:8000'
-
-    return {
-        provide: { 
-            axios: axios
-        },
-    }
-})
+  return {
+    provide: {
+      axios: axios,
+    },
+  };
+});
