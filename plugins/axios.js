6bb570ccdc8be7bda98a4dcb277683cecb55c047
Mikołaj Gawron <gawrmiko@gmail.com>
2024-05-18 19:29:42 +0200

    feat: remove withXSRFToken

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export default defineNuxtPlugin((NuxtApp) => {
   axios.defaults.withCredentials = true;
-  axios.defaults.withXSRFToken = true;
   axios.defaults.baseURL = "http://localhost:8000";
 
   return {
