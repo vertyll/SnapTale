c83f3d8bdd594591343ae3d9e4ef5a6a4ca0a11a
Mikołaj <gawrmiko@gmail.com>
2024-05-19 21:15:39 +0200

    style: change NuxtApp to nuxtApp

--- plugins/stores.js
+++ plugins/stores.js
@@ -2,7 +2,7 @@ import { useUserStore } from "~/stores/user";
 import { useProfileStore } from "~/stores/profile";
 import { useGeneralStore } from "~/stores/general";
 
-export default defineNuxtPlugin((NuxtApp) => {
+export default defineNuxtPlugin((nuxtApp) => {
   return {
     provide: {
       userStore: useUserStore(),
