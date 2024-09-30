f9feb8f99738c176c4f09ab9649fa4156ff9a41a
Mikołaj Gawron <gawrmiko@gmail.com>
2024-09-30 20:31:59 +0200

    feat: export cookies helpers to utils

--- components/CookieBanner.vue
+++ components/CookieBanner.vue
@@ -16,22 +16,7 @@
 
 <script setup>
 import { ref, onMounted } from 'vue';
-
-const setCookie = (name, value, days) => {
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    document.cookie = `${name}=${value};expires=${date.toUTCString()};path=/`;
-};
-
-const getCookie = (name) => {
-    const nameEQ = name + "=";
-    const ca = document.cookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i].trim();
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-};
+import { getCookie, setCookie } from '@/utils/cookies';
 
 const showBanner = ref(false);
 
---
+++ utils/cookies.js
@@ -0,0 +1,15 @@
+export const setCookie = (name, value, days) => {
+  const date = new Date();
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  document.cookie = `${name}=${value};expires=${date.toUTCString()};path=/`;
+};
+
+export const getCookie = (name) => {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i].trim();
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+};
