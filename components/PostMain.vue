1db89ff05b1ab20371f1be1f392ac9ae1f7bc220
Mikołaj Gawron <gawrmiko@gmail.com>
2024-09-30 21:36:33 +0200

    fix: fix refreshing

--- components/PostMain.vue
+++ components/PostMain.vue
@@ -1,7 +1,11 @@
 <template>
   <div :id="`PostMain-${post.id}`" class="flex border-b py-6">
     <div @click="isLoggedIn(post.user)" class="cursor-pointer">
-      <img class="rounded-full max-h-[60px]" width="60" :src="post.user.image" />
+      <img
+        class="rounded-full max-h-[60px]"
+        width="60"
+        :src="post.user.image"
+      />
     </div>
     <div class="pl-3 w-full px-4">
       <div class="flex items-center justify-between pb-0.5">
@@ -9,18 +13,22 @@
           <span class="font-bold hover:underline cursor-pointer">
             {{ $generalStore.allLowerCaseNoCaps(post.user.name) }}
           </span>
-          <span class="text-[13px] text-light text-gray-500 pl-1 cursor-pointer">
+          <span
+            class="text-[13px] text-light text-gray-500 pl-1 cursor-pointer"
+          >
             {{ post.user.name }}
           </span>
         </button>
 
-        <button @click="toggleFollow"
-          :class="isFollowing ? 'bg-gray-300 text-black' : 'border text-[#bc2cf0] hover:bg-[#ffeef2]'"
-          class="text-[15px] px-[21px] py-0.5 border-[#bc2cf0] font-semibold rounded-md">
-          {{ isFollowing ? 'Przestań śledzić' : 'Obserwuj' }}
+        <button
+          class="border text-[15px] px-[21px] py-0.5 border-[#bc2cf0] text-[#bc2cf0] hover:bg-[#ffeef2] font-semibold rounded-md"
+        >
+          Obserwuj
         </button>
       </div>
-      <div class="text-[15px] pb-0.5 break-words md:max-w-[400px] max-w-[300px]">
+      <div
+        class="text-[15px] pb-0.5 break-words md:max-w-[400px] max-w-[300px]"
+      >
         {{ post.text }}
       </div>
       <div class="text-[14px] text-gray-500 pb-0.5">
@@ -33,18 +41,36 @@
       </div>
 
       <div class="mt-2.5 flex">
-        <div @click="displayPost(post)"
-          class="relative min-h-[480px] max-h-[580px] max-w-[260px] flex items-center bg-black rounded-xl cursor-pointer">
-          <video v-if="post.video" ref="video" loop muted class="rounded-xl object-cover mx-auto h-full"
-            :src="post.video" />
-          <img class="absolute right-2 bottom-14" width="90" src="~/assets/images/snaptale-logo.png" />
+        <div
+          @click="displayPost(post)"
+          class="relative min-h-[480px] max-h-[580px] max-w-[260px] flex items-center bg-black rounded-xl cursor-pointer"
+        >
+          <video
+            v-if="post.video"
+            ref="video"
+            loop
+            muted
+            class="rounded-xl object-cover mx-auto h-full"
+            :src="post.video"
+          />
+          <img
+            class="absolute right-2 bottom-14"
+            width="90"
+            src="~/assets/images/snaptale-logo.png"
+          />
         </div>
         <div class="relative mr-[75px]">
           <div class="absolute bottom-0 pl-2">
             <div class="pb-4 text-center">
-              <button @click="isLiked ? unlikePost(post) : likePost(post)"
-                class="rounded-full bg-gray-200 p-2 cursor-pointer">
-                <Icon name="mdi:heart" size="25" :color="isLiked ? '#F02C56' : ''" />
+              <button
+                @click="isLiked ? unlikePost(post) : likePost(post)"
+                class="rounded-full bg-gray-200 p-2 cursor-pointer"
+              >
+                <Icon
+                  name="mdi:heart"
+                  size="25"
+                  :color="isLiked ? '#F02C56' : ''"
+                />
               </button>
               <span class="text-xs text-gray-800 font-semibold">
                 {{ post.likes.length }}</span>
@@ -73,7 +99,6 @@
 </template>
 
 <script setup>
-import { ref, computed, onMounted, onBeforeUnmount } from "vue";
 const { $generalStore, $userStore } = useNuxtApp();
 const props = defineProps(["post"]);
 const { post } = toRefs(props);
@@ -81,9 +106,8 @@ const { post } = toRefs(props);
 const router = useRouter();
 
 let video = ref(null);
-let isFollowing = ref(false);
 
-onMounted(async () => {
+onMounted(() => {
   const postElement = document.getElementById(`PostMain-${post.value.id}`);
   if (postElement) {
     let observer = new IntersectionObserver(
@@ -105,8 +129,6 @@ onMounted(async () => {
 
     observer.observe(postElement);
   }
-
-  isFollowing.value = await $userStore.checkIfFollowing(post.value.user.id);
 });
 
 onBeforeUnmount(() => {
@@ -166,22 +188,5 @@ const displayPost = (post) => {
   $generalStore.setBackUrl("/");
   $generalStore.selectedPost = null;
   setTimeout(() => router.push(`/post/${post.id}`), 200);
-};
-
-const toggleFollow = async () => {
-  if (!$userStore.id) {
-    $generalStore.isLoginOpen = true;
-    return;
-  }
-  try {
-    if (isFollowing.value) {
-      await $userStore.unfollowUser(post.value.user.id);
-    } else {
-      await $userStore.followUser(post.value.user.id);
-    }
-    isFollowing.value = !isFollowing.value;
-  } catch (error) {
-    console.log(error);
-  }
 };
 </script>
