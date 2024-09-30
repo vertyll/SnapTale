016bf3fc97a0acd20961cc9ce18cee55e9c4ec66
Mikołaj Gawron <gawrmiko@gmail.com>
2024-09-30 20:24:56 +0200

    feat: upate packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   pages: true,
-  modules: [
-    'nuxt-icon', 
-    '@pinia/nuxt', 
-    '@pinia-plugin-persistedstate/nuxt'
-  ],
-  css: ['~/assets/css/main.css'],
+  modules: ["nuxt-icon", "@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt"],
+  css: ["~/assets/css/main.css"],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   runtimeConfig: {
     public: {
       apiBaseUrl: process.env.API_BASE_URL,
-    }
-  }
+    },
+  },
+
+  compatibilityDate: "2024-09-30",
 });
